ae0075e7232f11cab8e9628f781c3548831f2c85
2017000135-IanEscobar <ianesco10@gmail.com>
2026-03-19 12:13:22 -0600

    Add files via upload
    Se Trabajo en Clase

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    //Verificar si un numero es positivo
+    // o negativo
+    private static void Main(string[] args)
+    {
+        //Entradas
+        Console.WriteLine("===NUMEROS POSITIVOS Y NEGATIVOS");
+        int num;
+        Console.Write("Dame un numero emtero: ");
+        num=Convert.ToInt32(Console.ReadLine());
+        //Proceso
+        //salidas
+        if (num >= 0)
+        {
+            Console.WriteLine("El numero es positivo");
+        }
+        else
+        {
+            Console.WriteLine("El numero es negativo");
+        }
+    }
+}
